7561a68af640d51efde814e7952799f2a12d2515
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-04 00:01:54 +0100

    adding a scene and cleaned up a bit

--- srcs/normal.c
+++ srcs/normal.c
@@ -11,9 +11,13 @@ void	surface_normal_sp(t_data *data, t_ray *ray, int *objid, t_shadow *shadow)
 void	surface_normal_pl(t_data *data, t_ray *ray, int *objid, t_shadow *shadow)
 {
 	(void) ray;
-	shadow->i_normal = vector_dev(shadow->intersection, \
-		data->elements->objects[*objid]->v_pos);
-	shadow->i_normal = normalise(shadow->i_normal);
+	shadow->intersection.x = 0.0;
+	shadow->intersection.y = 0.0;
+	shadow->intersection.z = 0.0;
+//	if (dot_prod(ray->v_direct, data->elements->objects[*objid]->v_orient) < 0)
+		shadow->i_normal = vec_mult(data->elements->objects[*objid]->v_orient, 1);
+//	else
+//		shadow->i_normal = vec_mult(data->elements->objects[*objid]->v_orient, -1);
 }
 
 void	surface_normal_cy(t_data *data, t_ray *ray, int *objid, t_shadow *shadow)
