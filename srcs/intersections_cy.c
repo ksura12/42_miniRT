156c7b1ab28badc2ecf0767927ac09b73946688d
ksura12 <kevin.sura@outlook.de>
2023-01-07 11:23:44 +0100

    norming intersection disk

--- srcs/intersections_cy.c
+++ srcs/intersections_cy.c
@@ -6,7 +6,7 @@
 /*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 05:25:01 by kaheinz           #+#    #+#             */
-/*   Updated: 2023/01/07 09:22:08 by ksura            ###   ########.fr       */
+/*   Updated: 2023/01/07 11:22:26 by ksura            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,29 @@
 static void	abc_calc(t_ray *ray, t_data *data, int i, float abc[3])
 {
 	t_vec	w;
+	t_obj	*obj;
 
+	obj = data->elements->objects[i];
 	abc[0] = dot_prod(ray->v_direct, ray->v_direct) \
-		- powf(dot_prod(ray->v_direct, data->elements->objects[i]->v_orient), 2);
-	w = vector_dev(ray->v_pos, data->elements->objects[i]->v_pos);
+		- powf(dot_prod(ray->v_direct, obj->v_orient), 2);
+	w = vector_dev(ray->v_pos, obj->v_pos);
 	abc[1] = 2 * (dot_prod(ray->v_direct, w) - dot_prod(ray->v_direct, \
-		data->elements->objects[i]->v_orient) * dot_prod(w, data->elements->objects[i]->v_orient));
-	abc[2] = dot_prod(w ,w) - powf(dot_prod(w, data->elements->objects[i]->v_orient), 2) \
-		- powf(data->elements->objects[i]->dia / 2, 2);
+		obj->v_orient) * dot_prod(w, obj->v_orient));
+	abc[2] = dot_prod(w, w) - powf(dot_prod(w, obj->v_orient), 2) \
+		- powf(obj->dia / 2, 2);
 }
 
-float	does_intersect_cy_disk(t_ray *ray, t_data *data, int i, int disk, int *objid)
+/**
+ * @brief checks if ray intersect with a cap of cylinder
+ * 
+ * 
+ * @param ray 
+ * @param data 
+ * @param io int * with io[0] = i and io[1] = objid
+ * @param disk 0 for lower cap, 1 for upper cap
+ * @return [float] t value if intersect, -1 if not
+ */
+float	does_intersect_cy_disk(t_ray *ray, t_data *data, int *io, int disk)
 {
 	float	den;
 	float	t;
@@ -33,7 +45,7 @@ float	does_intersect_cy_disk(t_ray *ray, t_data *data, int i, int disk, int *obj
 	t_vec	v;
 	t_obj	*cy;
 
-	cy = data->elements->objects[i];
+	cy = data->elements->objects[io[0]];
 	den = dot_prod(ray->v_direct, cy->v_orient);
 	if (!isequal(den, 0))
 	{
@@ -44,10 +56,11 @@ float	does_intersect_cy_disk(t_ray *ray, t_data *data, int i, int disk, int *obj
 		inters = vec_add(ray->v_pos, vec_mult(ray->v_direct, t));
 		v = vector_dev(inters, vec_add(cy->v_pos, vec_mult(cy->v_orient, \
 			cy->height * disk)));
-		if (sqrtf(dot_prod(v, v)) < (cy->dia / 2) && (t < ray->tmax || ray->tmax < 0))
+		if (sqrtf(dot_prod(v, v)) < (cy->dia / 2) && (t < ray->tmax \
+			|| ray->tmax < 0))
 		{
 			ray->tmax = t;
-			*objid = i;
+			io[1] = io[0];
 			ray->cy_cap = 1;
 			return (t);
 		}
@@ -60,9 +73,14 @@ int	does_intersect_cy_shadow(t_ray *ray, t_data *data, int i, int *objid)
 	float	tmp[2];
 	float	abc[3];
 	float	ret;
+	int		*io;
 
-	tmp[0] = find_min_value(does_intersect_cy_disk(ray, data, i, 0, objid), \
-		does_intersect_cy_disk(ray, data, i, 1, objid));
+	io = malloc(sizeof(int *));
+	io[0] = i;
+	io[1] = *objid;
+	tmp[0] = find_min_value(does_intersect_cy_disk(ray, data, io, 0), \
+		does_intersect_cy_disk(ray, data, io, 1));
+	free(io);
 	abc_calc(ray, data, i, abc);
 	if (islessequal(pow(abc[1], 2) - 4.0 * abc[0] * abc[2], 0) && tmp[0] < 0)
 		return(-1);
@@ -96,9 +114,15 @@ int	does_intersect_cy(t_ray *ray, t_data *data, int i, int *objid)
 	float	tmp[2];
 	float	abc[3];
 	float	ret;
+	int		*io;
 
-	tmp[0] = find_min_value(does_intersect_cy_disk(ray, data, i, 0, objid), \
-		does_intersect_cy_disk(ray, data, i, 1, objid));
+	io = malloc(sizeof(int *) * 2);
+	io[0] = i;
+	io[1] = *objid;
+
+	tmp[0] = find_min_value(does_intersect_cy_disk(ray, data, io, 0), \
+		does_intersect_cy_disk(ray, data, io, 1));
+	free(io);
 	abc_calc(ray, data, i, abc);
 	if (islessequal(pow(abc[1], 2) - 4.0 * abc[0] * abc[2], 0) && tmp[0] < 0)
 		return (-1);
